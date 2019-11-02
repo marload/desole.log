2ba4df495913a529efa56ef9578b5cb98c926c0c
olramde <rladhkstn8@gmail.com>
2019-11-03 04:23:17 +0900

    box refactoring

--- src/box.js
+++ src/box.js
@@ -2,28 +2,74 @@ const fitStringSize = (string, size, char) => {
     if (char === undefined) char = " ";
     return string + char.repeat(size - string.length);
 };
-// 39
+
+const twoColumnTopLinePrint = () => {
+    const twoColumnTopLineString = () => {
+        return (
+            "┌" +
+            "─".repeat(10) +
+            "DeSOLE.log" +
+            "─".repeat(18) +
+            "┬" +
+            "─".repeat(10) +
+            "┬" +
+            "─".repeat(27) +
+            "┐"
+        );
+    };
+    console.log(twoColumnTopLineString());
+};
+
+const twoColumnMiddleLinePrint = () => {
+    const twoColumnMiddleLineString = () => {
+        return (
+            "├" +
+            "─".repeat(10) +
+            "┼" +
+            "─".repeat(27) +
+            "┼" +
+            "─".repeat(10) +
+            "┼" +
+            "─".repeat(27) +
+            "┤"
+        );
+    };
+    console.log(twoColumnMiddleLineString());
+};
+
+const twoColumnTopLineString = () => {
+    return (
+        "┌" +
+        "─".repeat(10) +
+        "DeSOLE.log" +
+        "─".repeat(18) +
+        "┬" +
+        "─".repeat(10) +
+        "┬" +
+        "─".repeat(27) +
+        "┐"
+    );
+};
+
 const twoColumnFormat = (nameC1, valueC1, nameC2, valueC2) => {
     const indexSize = 10;
     const valueSize = 39 - 2 - 10;
-    let formatString = "";
 
-    formatString.concat(
-        "│",
-        fitStringSize(` ${nameC1}`, indexSize),
-        "│",
-        fitStringSize(` ${valueC1}`, valueSize),
-        "│",
-        fitStringSize(` ${nameC2}`, indexSize),
-        "│",
-        fitStringSize(` ${valueC2}`, valueSize),
+    return (
+        "│" +
+        fitStringSize(` ${nameC1}`, indexSize) +
+        "│" +
+        fitStringSize(` ${valueC1}`, valueSize) +
+        "│" +
+        fitStringSize(` ${nameC2}`, indexSize) +
+        "│" +
+        fitStringSize(` ${valueC2}`, valueSize) +
         "│"
     );
-    return formatString;
 };
 
 const headerBoxPrint = (type, name) => {
-    console.log(`┌${"─".repeat(9)}DeSOLE.log${"─".repeat(58)}┐`);
+    twoColumnTopLinePrint();
     if (name === undefined) {
         console.log(twoColumnFormat("Type", type, "Name", "$ Unknown $"));
     } else {
@@ -31,4 +77,5 @@ const headerBoxPrint = (type, name) => {
     }
 };
 
-console.log(headerBoxPrint("Number", "asd"));
+headerBoxPrint("Number", "asd");
+twoColumnMiddleLinePrint();
